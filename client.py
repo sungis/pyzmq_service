fdcd1ed1a121926d570df39fd3be142ab6ab7908
sungis <soisay@163.com>
2005-01-01 01:34:47 +0800

    同步

--- client.py
+++ client.py
@@ -5,13 +5,14 @@ import re
 import sys
 import json
 
+SERVER_ENDPOINT = "tcp://localhost:5555"
+#SERVER_ENDPOINT = "tcp://10.0.1.77:5555"
+CLIENT_IDENTITY = "AD_Client_pyzmq" 
+
 def send_request(header,q):
-  SERVER_ENDPOINT = "tcp://localhost:5555"
-#  SERVER_ENDPOINT = "tcp://10.0.1.77:5555"
-  CLIENT_IDENTITY = "AD_Client_pyzmq" 
   c=zmq.Context()
   s=c.socket(zmq.REQ)
-  s.set(zmq.IDENTITY,CLIENT_IDENTITY)
+#  s.set(zmq.IDENTITY,CLIENT_IDENTITY)
   s.connect(SERVER_ENDPOINT)
   m=[header,q]
   s.send_multipart(m)
@@ -20,23 +21,30 @@ def send_request(header,q):
 import threading
 from time import sleep,ctime
 
+url ='http://blog.csdn.net/grhunter/article/details/1203694'
+request='{"action":"adv","q":{"referurl":"'+url+'","keyword":[""]},"filter":{"city":[""],"province":[""]},"sort":1,"output":{"format":"json","offset":0,"size":30}}'
+m=['search',request]
 class advClient(threading.Thread):
     def __init__(self,id):
         threading.Thread.__init__(self)
-        SERVER_ENDPOINT = "tcp://10.0.1.77:5555"
-        #SERVER_ENDPOINT = "tcp://localhost:5555"
-        CLIENT_IDENTITY = "AD_Client_pyzmq" 
         c=zmq.Context()
         self.s=c.socket(zmq.REQ)
         self.s.set(zmq.IDENTITY,CLIENT_IDENTITY+'__'+str(id))
         self.s.connect(SERVER_ENDPOINT)
-        self.url ='http://blog.csdn.net/grhunter/article/details/1203694'
     def run(self):
         for i in range(100):
-            request='{"action":"adv","q":{"referurl":"'+self.url+'","keyword":[""]},"filter":{"city":[""],"province":[""]},"sort":1,"output":{"format":"json","offset":0,"size":30}}'
-            m=['search',request]
             self.s.send_multipart(m)
-            print self.s.recv()
+            rep = self.s.recv()
+            #print i
+
+def req2json(request):
+    data = send_request('search',request)
+    jdata = json.loads(data)
+    print 'totalCount',jdata["response"]['totalCount']
+    if jdata["response"]['totalCount']>0:
+        jobs = jdata["response"]["items"]
+        for i in jobs:
+            print i["jobTitle"]
 
 if __name__  == '__main__':
     action = sys.argv[1]
@@ -62,32 +70,26 @@ if __name__  == '__main__':
             print 'input url ==>'
             url = sys.stdin.readline()
             request='{"action":"adv","q":{"referurl":"'+url.strip()+'","keyword":[""]},"filter":{"city":[""],"province":[""]},"sort":1,"output":{"format":"json","offset":0,"size":30}}'
-            data = send_request('search',request)
-            jdata = json.loads(data)
-            print 'totalCount',jdata["response"]['totalCount']
-            if jdata["response"]['totalCount']>0:
-                jobs = jdata["response"]["items"]
-                for i in jobs:
-                    print i["jobTitle"]
+            req2json(request)
 
     elif action=='all':
-        request='{ "action" : "all" , "sort" : 1 , "output" : { "format" : "json" , "offset" : 0 , "size" : 10}}'
-        print send_request('search',request)
+        request='{ "action" : "all" , "sort" : 1 , "output" : { "format" : "json" , "offset" : 0 , "size" : 30}}'
+        req2json(request)
 
     elif action=='hunterjob':
-        request='{ "action" : "hunterjob" , "sort" : 1 , "output" : { "format" : "json" , "offset" : 0 , "size" : 10}}'
-        print send_request('search',request)
+        request='{ "action" : "hunterjob" , "sort" : 1 , "output" : { "format" : "json" , "offset" : 0 , "size" : 30}}'
+        req2json(request)
 
     elif action=='uniqueorgid':
-        request='{ "action" : "uniqueorgid" , "sort" : 1 , "output" : { "format" : "json" , "offset" : 0 , "size" : 10}}'
-        print send_request('search',request)
+        request='{ "action" : "uniqueorgid" , "sort" : 1 , "output" : { "format" : "json" , "offset" : 0 , "size" : 30}}'
+        req2json(request)
 
     elif action=='test':
         while(True):
             print 'input:'
             request = sys.stdin.readline()
             print request
-            print send_request('search',request)
+            req2json(request)
     elif action == 'thread':
         threads=[]
         n=100
@@ -101,7 +103,7 @@ if __name__  == '__main__':
     else:
         f = open('skill.txt')
         for k in f:
-            request='{ "action" : "searchJob" , "q" : { "keyword" : "'+k+'"} , "sort" : 1 , "output" : { "format" : "json" , "offset" : 0 , "size" : 10}}'
+            request='{ "action" : "searchJob" , "q" : { "keyword" : "'+k+'"} , "sort" : 1 , "output" : { "format" : "json" , "offset" : 0 , "size" : 30}}'
             print k
             print send_request('search',request)
             ch = sys.stdin.read(1)
